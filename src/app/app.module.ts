eb2be935630d9c2ddf1728479760bbe14ff06b5c
Michael <hitzker.michael@gmail.com>
2019-12-11 16:45:40 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpecificationComponent } from './specification/specification.component';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpecificationComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule
